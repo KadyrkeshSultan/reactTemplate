317da7e1fb6d5e2d1e9ff437b524f9010e7ec112
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-01-07 01:40:05 +0600

    Lesson 14

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={{backgroundColor: 'red', width: '100%'}}>
+            <Text>Text for View 1</Text>
+        </View>
+        <View style={styles.view2}>
+            <Text>Text for View 2</Text>
+        </View>
       </View>
     );
   }
@@ -15,7 +20,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+    marginTop: 20
   },
+  view2:{
+      backgroundColor: 'blue'
+  }
 });
