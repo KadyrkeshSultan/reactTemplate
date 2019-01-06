99f21d7ae23c7470d3484aa9b59fbf7ccb7e04a2
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-01-07 02:42:18 +0600

    Lesson 16

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Nav from './src/Nav/nav';
-import Generate from './src/Generator/generate'
+import Generate from './src/Generator/generate';
+import ListItem from './src/Generator/listitem'
 
 export default class App extends React.Component {
     state = {
-        nameOfApp: "My awesome app"
+        nameOfApp: "My awesome app",
+        random: [20, 13]
     }
 
-
+    onAddRandom = () =>{
+        const random = Math.floor(Math.random() *100) + 1;
+        this.setState(prevState =>{
+            return {
+                random : [...prevState.random, random]
+            };
+        });
+    }
 
     render() {
         return (
         <View style={styles.container}>
             <Nav nameOfApp={this.state.nameOfApp}/>
-            <Generate/>
+            <Generate add={this.onAddRandom}/>
+            
+            <ListItem items={this.state.random}/>
         </View>
         );
     }
--- src/Generator/generate.js
+++ src/Generator/generate.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 
-const generate = () => (
-    <View style={styles.generate}>
-        <Text>Add number</Text>
-    </View>
+const generate = (props) => (
+    <TouchableWithoutFeedback
+        onPress={() => {
+            props.add()
+        }}
+        //onLongPress={() => alert('Touched')}
+        //onPressIn={() => alert('Touched Press In')}
+        //onPressOut={() => alert('Touched Press Out')}
+        //delayLongPress={5000}
+    >
+        <View style={styles.generate}>
+            <Text>Add number</Text>
+        </View>
+    </TouchableWithoutFeedback>
 );
 
 const styles = StyleSheet.create({
@@ -12,7 +22,8 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center',
         backgroundColor: '#00bce4',
-        width: '100%'
+        width: '100%',
+        marginBottom: '10%'
     }
 });
 
